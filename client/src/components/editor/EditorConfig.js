b1fd1eee25da814b38388de8a96d4830465f0ff7
hyojelee <hyojelee1004ss@gmail.com>
2020-09-07 11:28:41 -0400

    ckeditor data added

--- client/src/components/editor/EditorConfig.js
+++ client/src/components/editor/EditorConfig.js
@@ -1,59 +1,30 @@
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-
 import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
-
 import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough";
-
 import Code from "@ckeditor/ckeditor5-basic-styles/src/code";
-
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
-
 import Link from "@ckeditor/ckeditor5-link/src/link";
-
 import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
-
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
-
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-
 import Font from "@ckeditor/ckeditor5-font/src/font";
-
 import Image from "@ckeditor/ckeditor5-image/src/image";
-
 import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
-
 import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-
 import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
-
 import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize";
-
 import List from "@ckeditor/ckeditor5-list/src/list";
-
 import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
-
 import Table from "@ckeditor/ckeditor5-table/src/table";
-
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
-
 import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
-
 import Indent from "@ckeditor/ckeditor5-indent/src/indent";
-
 import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock";
-
-/* import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter"; */
-
 import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter";
-
 import "@ckeditor/ckeditor5-build-classic/build/translations/ko";
-
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -174,7 +145,7 @@ export const editorConfiguration = {
 
         view: "p",
 
-        title: "original one",
+        title: "original",
 
         class: "ck-heading_paragraph",
       },
@@ -184,7 +155,7 @@ export const editorConfiguration = {
 
         view: "h1",
 
-        title: "header1",
+        title: "Header1",
 
         class: "ck-heading_heading1",
       },
@@ -194,7 +165,7 @@ export const editorConfiguration = {
 
         view: "h2",
 
-        title: "header2",
+        title: "Header2",
 
         class: "ck-heading_heading2",
       },
@@ -204,7 +175,7 @@ export const editorConfiguration = {
 
         view: "h3",
 
-        title: "header3",
+        title: "Header3",
 
         class: "ck-heading_heading3",
       },
--- client/src/routes/publicRoute/PostWrite.js
+++ client/src/routes/publicRoute/PostWrite.js
@@ -1,9 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Progress,
+  Button,
+} from "reactstrap";
+import { editorConfiguration } from "../../components/editor/EditorConfig";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Myinit from "../../components/editor/UploadFile";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PostWrite = () => {
+  const { isAuth } = useSelector((state) => state.auth);
+  const [form, setForm] = useState({ title: "", contents: "", fileUrl: "" });
+  const dispatch = useDispatch();
+
+  const onChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.value]: e.target.value,
+    });
+  };
+
+  const onSubmit = async (e) => {
+    await e.preventDefault();
+    const { title, contents, fileUrl, category } = form;
+  };
+
+  const getCKEditorData = (e, editor) => {
+    const data = editor.getData();
+    // get first img to card in main page
+    console.log(data);
+    if (data && data.match("<img src=")) {
+      const whereImg_start = data.indexOf("<img src=");
+      console.log(whereImg_start);
+      let whereImg_end = "";
+      let ext_name_find = "";
+      let result_Img_Url = "";
+
+      const ext_name = ["jpeg", "png", "jpg", "gif"];
+
+      for (let i = 0; i < ext_name.length; i++) {
+        if (data.match(ext_name[i])) {
+          console.log(data.indexOf(`${ext_name[i]}`));
+          ext_name_find = ext_name[i];
+          whereImg_end = data.indexOf(`${ext_name[i]}`);
+        }
+      }
+      console.log(ext_name_find);
+      console.log(whereImg_end);
+      if (ext_name_find === "jpeg") {
+        result_Img_Url = data.substring(whereImg_start + 10, whereImg_end + 4);
+      } else {
+        result_Img_Url = data.substring(whereImg_start + 10, whereImg_end + 3);
+      }
+      console.log("resultimgurl", result_Img_Url);
+      setForm({
+        ...form,
+        fileUrl: result_Img_Url,
+        contents: data,
+      });
+    } else {
+      // when no img when upload post
+      // get url from my aws
+      setForm({
+        ...form,
+        fileUrl: process.env.REACT_APP_BASIC_CARD_IMAGE_URL,
+        contents: data,
+      });
+    }
+  };
   return (
     <div>
-      <h1>postWrite </h1>
+      {isAuth ? (
+        <Form>
+          <FormGroup className="mb-3">
+            <Label for="title">Title</Label>
+            <Input
+              type="text"
+              name="title"
+              id="title"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+
+          <FormGroup className="mb-3">
+            <Label for="category">Category</Label>
+            <Input
+              type="text"
+              name="category"
+              id="category"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+
+          <FormGroup className="mb-3">
+            <Label for="title">Title</Label>
+            <CKEditor
+              editor={ClassicEditor}
+              config={editorConfiguration}
+              onInit={Myinit}
+              onBlur={getCKEditorData}
+            />
+            <Button color="dark" className="mt-3 col-md-2 offset-md-10 mb-3">
+              Submit
+            </Button>
+          </FormGroup>
+        </Form>
+      ) : (
+        <Col width={50} className="p-5 m-5">
+          <Progress animated color="info" value={100} />
+        </Col>
+      )}
     </div>
   );
 };
