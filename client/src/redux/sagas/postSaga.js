89d6c25b059a9ebff63bc96a03a2768cb1ef13d3
hyojelee <hyojelee1004ss@gmail.com>
2020-09-07 14:59:04 -0400

    postdetail page added

--- client/src/redux/sagas/postSaga.js
+++ client/src/redux/sagas/postSaga.js
@@ -2,6 +2,9 @@ import {
   POSTS_LOADING_FAILURE,
   POSTS_LOADING_SUCCESS,
   POSTS_LOADING_REQUEST,
+  POST_UPLOADING_SUCCESS,
+  POST_UPLOADING_FAILURE,
+  POST_UPLOADING_REQUEST,
 } from "../types";
 import axios from "axios";
 import { call, put, takeEvery, all, fork } from "redux-saga/effects";
@@ -30,6 +33,46 @@ function* loadPosts() {
 function* watchLoadPosts() {
   yield takeEvery(POSTS_LOADING_REQUEST, loadPosts);
 }
+
+//upload post
+
+const uploadPostAPI = (payload) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const token = payload.token;
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+  return axios.post("/api/post", payload, config);
+};
+function* uploadPosts(action) {
+  try {
+    const result = yield call(uploadPostAPI, action.payload);
+    console.log("uploadpost action", action);
+    console.log("uploadPostAPI, action.payload", result);
+
+    yield put({
+      type: POST_UPLOADING_SUCCESS,
+      payload: result.data,
+    });
+    // go to detail page after posting
+    yield put(push(`/post${result.data._id}`));
+  } catch (e) {
+    yield put({
+      type: POST_UPLOADING_FAILURE,
+      payload: e,
+    });
+    // go to home
+    yield push("/");
+  }
+}
+function* watchuploadPosts() {
+  yield takeEvery(POST_UPLOADING_REQUEST, uploadPosts);
+}
+
 export default function* postSaga() {
-  yield all([fork(watchLoadPosts)]);
+  yield all([fork(watchLoadPosts), fork(watchuploadPosts)]);
 }
--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -1,5 +1,8 @@
 import express from "express";
 import Post from "../../models/post";
+import Category from "../../models/category";
+import User from "../../models/user";
+
 import auth from "../../middleware/auth";
 const router = express.Router();
 // /api/post
@@ -8,6 +11,7 @@ import dotenv from "dotenv";
 import multerS3 from "multer-s3";
 import path from "path";
 import AWS from "aws-sdk";
+import { isNullOrUndefined } from "util";
 dotenv.config();
 
 const s3 = new AWS.S3({
@@ -53,14 +57,68 @@ router.get("/", async (req, res) => {
   res.json(postFindResult);
 });
 
-router.post("/", auth, async (req, res, next) => {
+router.post("/", auth, uploadS3.none(), async (req, res, next) => {
   try {
     console.log(req, "req");
-    const { title, contents, fileUrl, writer } = req.body;
-    const newPost = await Post.create({ title, contents, fileUrl, writer });
+    const { title, contents, fileUrl, writer, category } = req.body;
+    const newPost = await Post.create({
+      title,
+      contents,
+      fileUrl,
+      writer,
+      date: moment().format("MM-DD-YYYY hh:mm:ss"),
+    });
+    const findResult = await Category.findOne({
+      // categoryname from category model
+      categoryName: category,
+    });
+
+    console.log("findresult(category)", findResult);
+    ////if no category when post, make new category
+    if (isNullOrUndefined(findResult)) {
+      const newCategory = await Category.create({
+        categoryName: category,
+      });
+      await Post.findByIdAndUpdate(newPost._id, {
+        $push: { category: newCategory._id },
+      });
+      await Category.findByIdAndUpdate(newCategory._id, {
+        $push: { posts: newPost._id },
+      });
+      await User.findByIdAndUpdate(req.user.id, {
+        $push: {
+          posts: newPost._id,
+        },
+      });
+    } else {
+      // category exists
+      await Category.findByIdAndUpdate(findResult._id, {
+        $push: { posts: newPost._id },
+      });
+      await Post.findByIdAndUpdate(newPost._id, { category: findResult._id });
+      await User.findByIdAndUpdate(req.user.id, {
+        $push: { posts: newPost._id },
+      });
+    }
+    return res.redirect(`/api/post/${newPost._id}`);
     res.json(newPost);
   } catch (e) {
     console.log(e);
+  }
+});
+
+// post api/post/:id
+// go to detail page after posing successfully
+// public
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const post = await (await Post.findById(req.params.id))
+      .populate("writer", "firstname", "lastname")
+      .populate({ path: "category", select: "categoryName" });
+  } catch (e) {
+    console.error(e);
+    next();
   }
 });
 
