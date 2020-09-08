9c8a68398df11cb5db1aaacff9515cfd3dc846bb
hyojelee <hyojelee1004ss@gmail.com>
2020-09-08 03:51:42 -0400

    post detail added

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -4,13 +4,9 @@ import { Spinner, Row } from "reactstrap";
 const Loading = (
   <Fragment>
     <Row className="d-flex justify-content-center m-5">
-      <Spinner style={{ width: "3rem", height: "3rem" }} color="primary" />
-      <Spinner style={{ width: "3rem", height: "3rem" }} color="secondary" />
-      <Spinner style={{ width: "3rem", height: "3rem" }} color="success" />
-      <Spinner style={{ width: "3rem", height: "3rem" }} color="danger" />
-      <Spinner style={{ width: "3rem", height: "3rem" }} color="warning" />
-      <Spinner style={{ width: "3rem", height: "3rem" }} color="info" />
-      <Spinner style={{ width: "3rem", height: "3rem" }} color="dark" />
+      <Spinner style={{ width: "5rem", height: "5rem" }} color="primary" />
+      <Spinner style={{ width: "5rem", height: "5rem" }} color="secondary" />
+      <Spinner style={{ width: "5rem", height: "5rem" }} color="success" />
     </Row>
   </Fragment>
 );
--- client/src/routes/publicRoute/PostDetail.js
+++ client/src/routes/publicRoute/PostDetail.js
@@ -12,13 +12,16 @@ import {
 } from "../../redux/types";
 import { Link } from "react-router-dom";
 import { Row, Col, Button } from "reactstrap";
+import { BsPencilSquare, BsChatDotsFill, BsEyeFill } from "react-icons/bs";
+import BalloonEditor from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
+import { editorConfiguration } from "../../components/editor/EditorConfig";
 // initialstate post from reducer
 // initialstate auth from reducer
 
 const PostDetail = (req) => {
   const dispatch = useDispatch();
   // initialstate post from reducer
-  const { postDetail, creatorId, title, loading } = useSelector(
+  const { postDetail, writerId, title, loading } = useSelector(
     (state) => state.post
   );
   // initialstate auth from reducer
@@ -48,11 +51,11 @@ const PostDetail = (req) => {
   const EditBtn = (
     <Fragment>
       <Row className="d-flex justify-content-center pb-3">
-        <Col className="col-md-3 mr-md-3">
+        {/* <Col className="col-md-3 mr-md-3">
           <Link to="/" className="btn btn-dark btn-block">
             Home
           </Link>
-        </Col>
+        </Col> */}
         <Col className="col-md-3 mr-md-3">
           <Link
             to={`/post/${req.match.params.id}}`}
@@ -81,8 +84,71 @@ const PostDetail = (req) => {
       </Row>
     </Fragment>
   );
-  console.log(title);
-  return <div>detail</div>;
+
+  const Body = (
+    <>
+      {userId === writerId ? EditBtn : HomeBtn}
+      <Row className="border-bottom border-top border-dark p-3 mb-3 d-flex justify-content-between">
+        {(() => {
+          if (postDetail && postDetail.writer) {
+            return (
+              <Fragment>
+                <div
+                  style={{
+                    fontSize: "2.5rem",
+                    margin: "0 auto",
+                  }}
+                >
+                  <span style={{ marginRight: "2rem" }}>
+                    <Button color="info">
+                      {postDetail.category.categoryName}
+                    </Button>
+                  </span>
+                  {postDetail.title}
+                </div>
+                <div className="align-self-end">
+                  {postDetail.writer.firstname}
+                </div>
+              </Fragment>
+            );
+          }
+        })()}
+      </Row>
+      {postDetail && postDetail.comments ? (
+        <Fragment>
+          <div className="d-flex justify-content-end align-items-baseline small">
+            <BsPencilSquare />
+            &nbsp;
+            <span> {postDetail.date}</span>
+            &nbsp;&nbsp;
+            <BsChatDotsFill />
+            &nbsp;
+            <span>{postDetail.comments.length}</span>
+            &nbsp;&nbsp;
+            <BsEyeFill />
+            <span>{postDetail.views}</span>
+          </div>
+          <Row className="mb-3">
+            <CKEditor
+              editor={BalloonEditor}
+              data={postDetail.contents}
+              config={editorConfiguration}
+              disabled="true"
+            />
+          </Row>
+        </Fragment>
+      ) : (
+        <h1></h1>
+      )}
+    </>
+  );
+
+  return (
+    <div>
+      <Helmet title={`Post|${title}`} />
+      {loading === true ? Loading : Body}
+    </div>
+  );
 };
 
 export default PostDetail;
